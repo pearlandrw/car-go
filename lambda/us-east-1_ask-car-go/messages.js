cc7657314144b840c88340308eee4af786639037
Pearl Andrew <PAndrew@routeone.com>
2018-10-22 10:35:31 -0400

    git cmmit

--- lambda/us-east-1_ask-car-go/messages.js
+++ lambda/us-east-1_ask-car-go/messages.js
@@ -1,6 +1,6 @@
 const appName = "Car GO";
-const welcomeMessage = 'Hello and welcome to Car GO. The smart and easy way to browse cars for sale.';
-const welcomeReprompt = 'Welcome. My job is to help you find a car. I am ready when you are';
+const welcomeMessage = 'Hello and welcome to Car GO. The smart and easy way to browse cars for sale';
+const welcomeReprompt = 'My job is to help you find a car. I am ready when you are';
 const helpOutput = 'With car GO, you can get the best deal for your buck. For example, you could say "Get me a deal for 2018 Honda CRV". Now, which year-make-model are you interested in?';
 const connectError = 'There is a problem connecting to MarketCheck at this time. Please try again later.';
 
